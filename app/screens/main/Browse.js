d4746168147d661208c7709d81cc47ed6b8e8e21
haffjjj <hafizjoundys@gmail.com>
2018-07-11 22:49:55 +0700

    refactoring

--- app/screens/main/Browse.js
+++ app/screens/main/Browse.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react'
 import {Text, FlatList, StyleSheet, Image,View, Dimensions, StatusBar, AsyncStorage} from 'react-native'
 
-import {Container,ListItem, Body, Button, Icon, Spinner} from 'native-base'
+import {Container,ListItem, Body, Button, Icon} from 'native-base'
 
 import { Toolbar } from 'react-native-material-ui'
 import { ThemeProvider } from 'react-native-material-ui'
@@ -10,6 +10,8 @@ import axios from 'axios'
 
 const screenWidth = Dimensions.get('window').width
 const imgResize = screenWidth/65.25
+const errorMessage = "Maaf terjadi kesalahan"
+const rows = 15
 
 export default class App extends Component{
 
@@ -19,33 +21,33 @@ export default class App extends Component{
         bookmarks: [],
         startPage: 0,
         onSearch: false,
-        refresh: false
+        isLoading: false
     }
 
     componentDidMount(){
         this._retrieveData()
 
         this.setState({
-            refresh: true
+            isLoading: true
         })
 
-        this.onLoad(this.state.startPage);
+        this.getMangaData(this.state.startPage);
     }
 
-    onLoad = (startPage, reset = false)=>{
+    getMangaData = (startPage, reset = false)=>{
         axios({
             method: 'POST',
-            url: 'http://192.168.43.142/api/get_mangas.php',
-            // url: 'http://192.168.56.1/api/get_mangas.php',
+            // url: 'http://192.168.43.142/api/get_mangas.php',
+            url: 'http://192.168.56.1/api/get_mangas.php',
             headers: { 'content-type': 'application/x-www-form-urlencoded' },
             data: {
               start: startPage,
-              rows: 5,
+              rows,
               key: 'da3a9900-5c2e-4ee1-a660-94929dddf08e'
             }
         }).then((result)=>{
             this.setState({
-                refresh:false
+                isLoading:false
             })
 
             if(result.data.result == true){
@@ -56,35 +58,35 @@ export default class App extends Component{
                     this.setState({
                         data,
                         startPage: this.state.startPage + 5,
-                        refresh: false
+                        isLoading: false
                     })
                 }
                 else{
                     this.setState({
                         data: result.data.data,
                         startPage: 0,
-                        refresh: false
+                        isLoading: false
                     })
                 }
             }
             else{
-                alert('Terjadi Kesalahan :(')
+                alert(errorMessage)
             }
 
         }).catch(()=>{
-            alert('Terjadi Kesalahan :(')
+            alert(errorMessage)
         })
     }
 
     handleSearch = ()=>{
         this.setState({
-            refresh: true
+            isLoading: true
         })
 
         axios({
             method: 'POST',
-            url: 'http://192.168.43.142/api/get_mangas.php',
-            // url: 'http://192.168.56.1/api/get_mangas_where.php',
+            // url: 'http://192.168.43.142/api/get_mangas.php',
+            url: 'http://192.168.56.1/api/get_mangas_where.php',
             headers: { 'content-type': 'application/x-www-form-urlencoded' },
             data: {
                 where: this.state.search,
@@ -93,7 +95,7 @@ export default class App extends Component{
         }).then((result)=>{
             this.setState({
                 data: result.data.data,
-                refresh:false
+                isLoading:false
             })
         })
     }
@@ -115,39 +117,35 @@ export default class App extends Component{
     handleOnPull = ()=>{
         if(this.state.onSearch == false){
             this.setState({
-                refresh: true,
+                isLoading: true,
             })
     
-            this.onLoad(0,true)
+            this.getMangaData(0,true)
         }
     }
 
     handleOnReach = ()=>{
         if(this.state.onSearch == false){
             this.setState({
-                refresh:true
+                isLoading:true
             })
-            this.onLoad(this.state.startPage)
+            this.getMangaData(this.state.startPage)
         }
     }
 
     handleOnSearchBack = ()=>{
         this.setState({
-            refresh: true,
+            isLoading: true,
             onSearch: false
         })
 
-        this.onLoad(0,true)
+        this.getMangaData(0,true)
     }
-
-
-    // AsyncStoragerr
 
     _retrieveData = async () => {
         try {
           const value = await AsyncStorage.getItem('mangaBookmarks')
           if (value !== null) {
-            // We have data!!
             this.setState({
                 bookmarks: JSON.parse(value)
             })
@@ -167,76 +165,40 @@ export default class App extends Component{
         }
     }
 
-    // endAsyncStorage
-    
     _keyExtractor = (item, index) => item.id
 
     _onPressItem = () => {
         alert('list onpress')
     }
-    
-
 
     _renderItem = ({item}) => (
         <ListItem onPress={()=>this.props.navigation.navigate('MangaDetails')}>
             <Body>
-                <View style={{flex: 1,flexDirection: 'row'}}>
+                <View style={styles.listWrapper}>
 
-                    <View style={{flex: 5, flexDirection: 'row',}}>
-                            <Image
-                                style={{
-                                    width: imgResize*20, 
-                                    height: imgResize*30,
-                                    borderRadius: 8
-                                }}
-                                source={{uri: item.img}}
-                            />
-                        <View style={{
-                            paddingLeft: 5,
-                            paddingRight: 5,
-                        }}>
-                            <Text style={{
-                                color: '#121212'
-                            }}>{item.title}</Text>
-                            <View  style={{
-                                backgroundColor: '#568BF2',
-                                borderRadius: 50,
-                                width: 35,
-                                marginTop: 2,
-                                marginBottom: 10
-                            }}>
-                                <Text style={{
-                                    textAlign: 'center',
-                                    color: 'white',
-                                    fontSize: 11,
-                                }}>{item.score}</Text>
+                    <View style={styles.listItem}>
+                        <Image
+                            style={styles.listImage}
+                            source={{uri: item.img}}
+                        />
+                        <View style={styles.listItemContent}>
+                            <Text style={styles.listItemTitle}>{item.title}</Text>
+                            <View  style={styles.listItemScoreWrapper}>
+                                <Text style={styles.listItemScore}>{item.score}</Text>
                             </View>
-                            <Text style={{
-                                fontSize: 12,
-                                color: '#878787'
-                            }}>Popularity #{item.popularity}</Text>
-                            <Text style={{
-                                fontSize: 12,
-                                color: '#878787'
-                            }}>Ranked #{item.ranked}</Text>
+                            <Text style={styles.listItemPopularity}>Popularity #{item.popularity}</Text>
+                            <Text style={styles.listItemRanked}>Ranked #{item.ranked}</Text>
                         </View>
                     </View>
-                    <Button transparent onPress={()=>this.handleBookmark(item.id)} style={{
-                        alignSelf: 'center',
-                        flex: 1
-                    }}>
-                    
-                        <Icon style={{
-                            color: 'red',
-                            fontSize: 15
-                        }}  name={this.state.bookmarks.includes(item.id) == true ? 'ios-heart' : 'ios-heart-outline'} />
+
+                    <Button transparent onPress={()=>this.handleBookmark(item.id)} style={styles.listItemBookmark}>
+                        <Icon style={styles.listItemBookmarkIcon}  name={this.state.bookmarks.includes(item.id) == true ? 'ios-heart' : 'ios-heart-outline'} />
                     </Button>
+
                 </View>
             </Body>
         </ListItem>
     )
-
-    // endFlatlist
 
     render(){
         return(
@@ -269,17 +231,18 @@ export default class App extends Component{
                         onRightElementPress={ (label) => { alert(JSON.stringify(label)) }}
                     />
                 </ThemeProvider>
-                {/* <Text>{JSON.stringify(this.state.data)}</Text> */}
-                <FlatList
-                    refreshing = {this.state.refresh}
+
+                
+
+                {/* <FlatList
+                    refreshing = {this.state.isLoading}
                     onRefresh = {this.handleOnPull}
                     onEndReachedThreshold = {0.1}
                     onEndReached = {this.handleOnReach}
                     data={this.state.data}
-                    // extraData={this.state.data}
                     keyExtractor={this._keyExtractor}
                     renderItem={this._renderItem}
-                />
+                /> */}
             </Container>
         )
     }
@@ -288,5 +251,53 @@ export default class App extends Component{
 const styles = StyleSheet.create({
     mainWrapper: {
         backgroundColor: 'white'
+    },
+    listWrapper:{
+        flex: 1,
+        flexDirection: 'row'
+    },
+    listItem:{
+        flex: 5, 
+        flexDirection: 'row'
+    },
+    listImage: {
+        width: imgResize*20, 
+        height: imgResize*30,
+        borderRadius: 8
+    },
+    listItemContent: {
+        paddingLeft: 5,
+        paddingRight: 5,
+    },
+    listItemTitle: {
+        color: '#121212'
+    },
+    listItemScoreWrapper: {
+        backgroundColor: '#568BF2',
+        borderRadius: 50,
+        width: 35,
+        marginTop: 2,
+        marginBottom: 10
+    },
+    listItemScore: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 11,
+    },
+    listItemPopularity: {
+        fontSize: 12,
+        color: '#878787'
+    },
+    listItemRanked:{
+        fontSize: 12,
+        color: '#878787'
+    },
+    listItemBookmark: {
+        alignSelf: 'center',
+        flex: 1
+    },
+    listItemBookmarkIcon:{
+        color: 'red',
+        fontSize: 15
     }
 })
